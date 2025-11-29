7cd37d8c33f2243f1dac4b327096bb2dc708662b
LoulouPlou <leia.plourde@hotmail.com>
2025-11-29 13:20:02 -0500

    adding back dialogue and scene manager to game

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    private SceneController _sceneController; // manage les scenes avec ceci pour fadeout
+
+    [Header("Nom de la scène de jeu")]
+    public string gameSceneName = "SampleScene";
+    public string menuSceneName = "Scenes/StartScene";
+    // Assignée au bouton "Jouer"
+    public void OnPlay()
+    {
+        if (!string.IsNullOrEmpty(gameSceneName))
+        {
+            _sceneController.LoadScene(gameSceneName);
+        }
+    }
+
+    // Assignée au bouton "Menu"
+    public void OnMenu()
+    {
+        if (!string.IsNullOrEmpty(menuSceneName))
+        {
+            _sceneController.LoadScene(menuSceneName);
+        }
+    }
+
+    //Assignée au bouton "Quitter"
+    public void OnQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+    }
+}
